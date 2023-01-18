e6eebdc27de966a4190d0f8a52c5d2987c2c8f82
Dellavrite <delavrite@gmail.com>
2023-01-18 13:56:52 +0300

    hot fix gitlab connection

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 sh ./getenv.sh
-scp ./.env $SERVER_USER@$SERVER_IP:/
+scp ./.env $SERVER_USER@$SERVER_IP:/home/dell-dev/
 ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "docker login -u gitlab-ci-token -p $CI_BUILD_TOKEN $CI_REGISTRY"
 ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "docker pull $TAG_COMMIT"
 ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "docker container rm -f backend || true"
-ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "docker run --env-file=/.env -d -p 8000:8000 --name backend $TAG_COMMIT"
+ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "docker run --env-file=/home/dell-dev/.env -d -p 8000:8000 --name backend $TAG_COMMIT"
