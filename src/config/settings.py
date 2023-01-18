33f616026ae74ffdbbe21e49391149ce015e42a4
Dellavrite <delavrite@gmail.com>
2023-01-18 11:38:01 +0300

    hot fix gitlab connection

--- src/config/settings.py
+++ src/config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import gitlab, os
-gl = gitlab.Gitlab(job_token=os.environ['CI_JOB_TOKEN'])
+gl = gitlab.Gitlab(private_token="glpat-BndEPDd2JKag7aXLRMp9")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
