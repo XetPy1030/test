a495eab764f49f7740671deeecbf9a19aaaf21bc
Dellavrite <delavrite@gmail.com>
2023-01-18 17:04:22 +0300

    add local version

--- src/config/settings.py
+++ src/config/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if MODE == "prod":
+if MODE == "dev":
     DATABASES = {
         'default': {
             "ENGINE": "django.db.backends.postgresql",
