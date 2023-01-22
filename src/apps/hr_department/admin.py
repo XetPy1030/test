46722d0e46a931ae4ea2bc3a0f5bfcb4de742cb2
XetPy1030 <xetpy1030@mail.ru>
2023-01-23 02:24:53 +0300

    git ignore

--- src/apps/hr_department/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from apps.hr_department.models import ServerEmployeeInformation, DraftEmployeeInformation
-
-# Register your models here.
-
-admin.site.register(ServerEmployeeInformation)
-admin.site.register(DraftEmployeeInformation)
--- src/apps/hr_department/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HrDepartmentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.hr_department'
