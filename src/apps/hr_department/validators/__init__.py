00467172f0adf851b5b7bd00015c939fa13c2336
XetPy1030 <xetpy1030@mail.ru>
2023-01-23 03:35:30 +0300

    edit code

--- src/apps/hr_department/validators/__init__.py
+++ src/apps/hr_department/validators/__init__.py
@@ -1,1 +1,2 @@
-from apps.hr_department.validators.validators import JwtTokenValidator
+from apps.hr_department.validators.jwt_validators import JwtTokenValidator, AdminJwtTokenValidator
+from apps.hr_department.validators.fields_validators import INNValidator, SNILSValidator, PassportValidator
--- src/apps/hr_department/views.py
+++ src/apps/hr_department/views.py
@@ -14,11 +14,13 @@ from apps.hr_department.utils.search_engine import search_by_full_name
 
 class FormDraftHandler(APIView):
     """
-    Форма для заполнения информации о сотруднике.
+    Форма для заполнения черновика информации о сотруднике.
     Отправляется фронтендом.
     Сериализуется в модель DraftEmployeeInformation.
     И сохраняется в БД если валидация прошла успешно.
     Нужно для того, чтобы сотрудник мог сохранить свою форму и вернуться к ней позже.
+    owner_id - id сотрудника, который заполняет форму.
+    user_id - id сотрудника, которого заполняют.
     """
 
     @staticmethod
@@ -49,4 +51,27 @@ class SearchHandler(APIView):
         full_name = request.GET.get('full_name')
         server_employee_information = search_by_full_name(full_name)
         serializer = DraftEmployeeInformationSerializer(server_employee_information, many=True)
+        return HttpResponse(serializer.data)
+
+
+class FormSaveEmployeeHandler(APIView):
+    """
+    Сохраняет форму сотрудника в БД.
+    """
+
+    @staticmethod
+    def post(request):
+        serializer = DraftEmployeeInformationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return HttpResponse(status=201)
+        return HttpResponse(status=400)
+
+    @staticmethod
+    def get(request):
+        """
+        Возвращает список всех полей модели DraftEmployeeInformation в виде json.
+        """
+        model = DraftEmployeeInformation.objects.all()[0]  # TODO: get the object by id from request
+        serializer = DraftEmployeeInformationSerializer(model)
         return HttpResponse(serializer.data)
