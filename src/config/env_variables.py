ff39729559bb011ae4dfa42cbe2e260214cc6393
Dellavrite <delavrite@gmail.com>
2023-01-25 00:50:38 +0300

    auto

--- src/config/env_variables.py
+++ src/config/env_variables.py
@@ -1,6 +1,6 @@
 import os
 
-DATA_FOLDER = os.environ.get("data_folder")
+DATA_FOLDER = os.environ.get("data_folder", "./data/")
 MODE = os.environ.get("mode", "local")
 if MODE == "local":
     PICTURES_FOLDER = "./pictures"
