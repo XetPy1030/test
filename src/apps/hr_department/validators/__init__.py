d23074805aa2edc6fa1d080393cde403228891d6
Dellavrite <delavrite@gmail.com>
2023-01-24 15:22:14 +0000

    Revert "Merge branch 'dell_db' into 'dev'"

--- src/apps/hr_department/validators/__init__.py
+++
@@ -1,2 +0,0 @@
-from apps.hr_department.validators.jwt_validators import JwtTokenValidator, AdminJwtTokenValidator
-from apps.hr_department.validators.fields_validators import INNValidator, SNILSValidator, PassportValidator
