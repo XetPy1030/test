41e7012973db30b102ed0905edd229f4c9c01c6c
Dellavrite <delavrite@gmail.com>
2023-01-25 00:32:55 +0300

    auto

--- src/config/env_variables.py
+++ src/config/env_variables.py
@@ -1,12 +1,11 @@
 import os
 
 DATA_FOLDER = os.environ.get("data_folder")
-if not DATA_FOLDER:
+MODE = os.environ.get("mode", "local")
+if MODE == "local":
     PICTURES_FOLDER = "./pictures"
-    MODE = 'local'
     DATABASE_PASSWORD = None
 else:
     PICTURES_FOLDER = DATA_FOLDER + "pictures"
     DATABASE_PASSWORD = os.environ.get("database_passwd")
-    MODE = os.environ.get("mode")
     print("MODE: ", MODE)
