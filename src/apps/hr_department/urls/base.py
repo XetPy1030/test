8c299459fd1df3e613d318dbbdb53db6f7264e80
Dellavrite <delavrite@gmail.com>
2023-01-27 14:22:47 +0300

    Resolve conflicts

--- src/apps/hr_department/urls/base.py
+++ src/apps/hr_department/urls/base.py
@@ -1,8 +1,5 @@
-from django.urls import path, include, re_path
-from rest_framework.routers import DefaultRouter
-
+from django.urls import path, include
 from apps.hr_department import views
-from config.env_variables import MODE
 
 urlpatterns_user = [
     path('draft/', views.UserDraftEmployeeHandler.as_view()),
@@ -14,18 +11,9 @@ urlpatterns_admin = [
     path('search/', views.SearchHandler.as_view()),  # TODO
 ]
 
-if MODE != 'local':
-    router = DefaultRouter()
-    search = router.register(r'search_employers',
-                            views.ServerSearchEmployeeInformationDocumentViewSet,
-                            basename='searversearchdocument')
-
 urlpatterns = [
     path('user/', include(urlpatterns_user)),
     path('admin/', include(urlpatterns_admin)),
     path('test/', views.test)
 ]
-
-if MODE != 'local':
-    urlpatterns += router.urls
 
---
+++ src/config/settings/dev.py
@@ -0,0 +1,26 @@
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+ELASTIC_SEARCH_HOST = 'es01'
+ELASTIC_SEARCH_PORT = 9200
+
+ELASTICSEARCH_DSL = {
+    'default': {
+        'hosts': 'es01:9200'
+    },
+}
+
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "hrdb",
+        "USER": "master",
+        "PASSWORD": "9D9*1$!kVZRo",
+        "HOST": "postgres",
+        "PORT": 5432,
+    }
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
+
