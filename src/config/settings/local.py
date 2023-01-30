3cbd3628caac91c10421a23ee233a390a105d2ab
Dellavrite <delavrite@gmail.com>
2023-01-30 11:48:11 +0300

    fix local

--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -2,9 +2,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-ELASTIC_SEARCH_HOST = 'es01'
-ELASTIC_SEARCH_PORT = 9200
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -13,13 +10,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'localdb.sqlite3',
     }
-}
-
-ELASTICSEARCH_DSL = {
-    'default': {
-        'hosts': 'es01:9200',
-        'timeout': 60,
-    },
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
