6bb3b82922a6a5774e95543fc61dc430d16210fb
Dellavrite <delavrite@gmail.com>
2023-02-15 13:28:42 +0300

    Fix db

--- src/apps/hr_department/utils/get_all_fields.py
+++ src/apps/hr_department/utils/get_all_fields.py
@@ -1,6 +1,9 @@
 def get_all_fields_for_document(model):
     fields_list = []
     for field in model._meta.fields:
+        # if type of django field == ImageField or FileField, then skip
+        if type(field).__name__ == 'ImageField' or type(field).__name__ == 'FileField':
+            continue
         if field.name == 'id':
             continue
         fields_list.append(field.name)
