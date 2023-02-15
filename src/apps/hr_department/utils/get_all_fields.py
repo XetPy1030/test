f68a9d50784fcbc5c5fe3e4dca7f4d7791935c3b
Dellavrite <delavrite@gmail.com>
2023-02-15 12:55:57 +0300

    Fix db

--- src/apps/hr_department/utils/get_all_fields.py
+++ src/apps/hr_department/utils/get_all_fields.py
@@ -1,5 +1,7 @@
 def get_all_fields_for_document(model):
     fields_list = []
     for field in model._meta.fields:
+        if field.name == 'id':
+            continue
         fields_list.append(field.name)
     return fields_list
